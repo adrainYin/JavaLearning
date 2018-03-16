58c3b4108d60b7515e0077a22994b792248f7b7e
yinchenhao <ych19941103@163.com>
2018-03-16 23:23:45 +0800

    3

--- src/MemoryCopy.java
+++ src/MemoryCopy.java
@@ -24,15 +24,44 @@ public class MemoryCopy {
                 new File("src" + File.separator + "testA.txt"),
                 new File("src" + File.separator + "testB.txt")
         };
-        String data[] = new String[file.length];
-        for (int i = 0; i < file.length; i++) {
-            data[i] = readFile(file[i]);
-        }
+//        String data[] = new String[file.length];
+////        for (int i = 0; i < file.length; i++) {
+////            data[i] = readFile(file[i]);
+////        }
+////        StringBuffer stringBuffer = new StringBuffer();
+////        String dataA[] = data[0].split(" ");
+////        String dataB[] = data[1].split(" ");
+////        for (int i = 0; i < dataA.length; i++) {
+////            stringBuffer.append(dataA[i]).append("(").append(dataB[i]).append(")").append(" ");
+////        }
+////        System.out.println(stringBuffer);
+        //打印流
+//        /**
+//         * PringStream打印流只能接收OutputStream对象，并且不能打印二进制数据
+//         */
+//        PrintStream printStream = new PrintStream(new FileOutputStream(file[0]));
+//        printStream.println(1234);
+//        printStream.println("hello world");
+//
+//        PrintWriter printWriter = new PrintWriter(new FileOutputStream(file[1]));
+//        printWriter.write(12346);
+//        printWriter.write("\r\n");
+//        printWriter.write("nihaoshijie!!!");
+
+        OutputStream outputStream = System.out;
+        String string = "hello world";
+        byte data[] = string.getBytes();
+        outputStream.write(data);
+
+        InputStream inputStream = System.in;
         StringBuffer stringBuffer = new StringBuffer();
-        String dataA[] = data[0].split(" ");
-        String dataB[] = data[1].split(" ");
-        for (int i = 0; i < dataA.length; i++) {
-            stringBuffer.append(dataA[i]).append("(").append(dataB[i]).append(")").append(" ");
+        int temp;
+        while ((temp = inputStream.read()) != -1){
+            if (temp == '\n'){
+                break;
+            }
+
+            stringBuffer.append((char) temp);
         }
         System.out.println(stringBuffer);
     }
