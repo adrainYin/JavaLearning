75c8368fb3b6752f280bc2cc88875e298e4fc0aa
AdrainYin <ych19941103@163.com>
2018-06-04 09:07:17 +0800

    up-to-date

--- src/thread/Run.java
+++ src/thread/Run.java
@@ -1,6 +1,7 @@
 package thread;
 
-import java.util.concurrent.atomic.AtomicInteger;
+
+import java.util.Calendar;
 
 public class Run {
     public static void main(String[] args) {
@@ -11,6 +12,33 @@ public class Run {
 //        for (int i = 0; i < 100; i++) {
 //            new Thread(new ATestThread()).start();
 //        }
+        Service service = new Service();
+        Runnable runnable = () -> {
+            System.out.println(Thread.currentThread().getName() + "调用的时间为" +
+            System.currentTimeMillis());
+            service.waitMethod();
+        };
+
+        Runnable runnable1 = () -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "调用的时间为" +
+                    System.currentTimeMillis());
+            service.waitMethod();
+        };
+
+        Thread thread = new Thread(runnable);
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable);
+        thread.setName("A");
+        thread1.setName("B");
+        thread2.setName("C");
+        thread.start();
+        thread2.start();
+        thread1.start();
 
 
     }
--- src/thread/Service.java
+++ src/thread/Service.java
@@ -1,31 +1,92 @@
 package thread;
 
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Service {
-    public static synchronized void printA(){
+//    public static synchronized void printA(){
+//        try {
+//            System.out.println("线程名为" + Thread.currentThread().getName() + "在" +
+//                    System.currentTimeMillis() + "调用方法A");
+//            Thread.sleep(3000);
+//            System.out.println("线程名为" + Thread.currentThread().getName() + "在" +
+//                    System.currentTimeMillis() + "离开方法A");
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static synchronized void printB(){
+//        System.out.println("线程名为" + Thread.currentThread().getName() + "在" +
+//                System.currentTimeMillis() + "调用方法B");
+//        System.out.println("线程名为" + Thread.currentThread().getName() + "在" +
+//                System.currentTimeMillis() + "离开方法B");
+//    }
+//
+//    public synchronized void printC(){
+//        System.out.println("线程名为" + Thread.currentThread().getName() + "在" +
+//                System.currentTimeMillis() + "调用方法C");
+//        System.out.println("线程名为" + Thread.currentThread().getName() + "在" +
+//                System.currentTimeMillis() + "离开方法C");
+//    }
+
+    /**
+     * 测试getQueueLength(Condition condition)方法
+     * boolean hasQueueThread(Thread thread)测试当前线程是否在等待锁
+     * boolean hasQueueThreads()测试是否有线程在等待此锁
+     */
+    private ReentrantLock lock = new ReentrantLock();//如果不加boolean值，那么默认new的锁是非公平锁
+    private Condition condition = lock.newCondition();
+
+//    public void waitMethod(){
+//        lock.lock();
+//        try {
+//            condition.await();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } finally {
+//            lock.unlock();
+//        }
+//    }
+//
+//    public void notofyMethod(){
+//        try {
+//            lock.lock();
+//            System.out.println("有" + lock.getWaitQueueLength(condition) + "个线程在等待condition的notify");
+//            condition.notify();
+//        } finally {
+//            lock.unlock();
+//        }
+//    }
+
+    public void waitMethod(){
         try {
-            System.out.println("线程名为" + Thread.currentThread().getName() + "在" +
-                    System.currentTimeMillis() + "调用方法A");
-            Thread.sleep(3000);
-            System.out.println("线程名为" + Thread.currentThread().getName() + "在" +
-                    System.currentTimeMillis() + "离开方法A");
+            /**
+             * 如果第一次锁没有争用则立即会获得锁，如果锁被用则会等待一定时间
+             * 等待时间内线程处于阻塞状态，如果阻塞期间能获得锁则立即获得结束阻塞
+             * 若期间内没有获得锁则立即结束阻塞返回false
+             * 若等待等待期间中断则立即抛出中断异常
+             */
+            if (lock.tryLock(3, TimeUnit.SECONDS)) {
+                System.out.println("  " + Thread.currentThread().getName() + "获得锁的时间为" +
+                        System.currentTimeMillis());
+                Thread.sleep(10000);
+            } else {
+                System.out.println("   " + Thread.currentThread().getName() + "没有获得锁");
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            if (lock.isHeldByCurrentThread()){
+                lock.unlock();
+            }
         }
     }
 
-    public static synchronized void printB(){
-        System.out.println("线程名为" + Thread.currentThread().getName() + "在" +
-                System.currentTimeMillis() + "调用方法B");
-        System.out.println("线程名为" + Thread.currentThread().getName() + "在" +
-                System.currentTimeMillis() + "离开方法B");
-    }
 
-    public synchronized void printC(){
-        System.out.println("线程名为" + Thread.currentThread().getName() + "在" +
-                System.currentTimeMillis() + "调用方法C");
-        System.out.println("线程名为" + Thread.currentThread().getName() + "在" +
-                System.currentTimeMillis() + "离开方法C");
-    }
 
 }
