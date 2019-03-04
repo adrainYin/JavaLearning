7a4e362b1bb5dcdecc6f06cd84686e4f0fc5f3d2
AdrainYin <ych19941103@163.com>
2019-03-04 17:21:01 +0800

    up-to-date

--- src/net/ClientDemo.java
+++ src/net/ClientDemo.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class ClientDemo {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("127.0.0.1",9999);
+        Socket socket = new Socket("59.78.194.236",55333);
         Scanner scanner = new Scanner(socket.getInputStream());
         while (scanner.hasNext()){
             System.out.println(scanner.next());
