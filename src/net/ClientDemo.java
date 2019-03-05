b61a4f0a2850ab41419b09a5709c347e41a89b0f
AdrainYin <ych19941103@163.com>
2019-03-05 14:33:30 +0800

    update NIO

--- src/net/ClientDemo.java
+++ src/net/ClientDemo.java
@@ -1,7 +1,13 @@
 package net;
 
+import thread.Client;
+
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
 import java.util.Scanner;
 
 public class ClientDemo {
@@ -13,5 +19,19 @@ public class ClientDemo {
         }
         scanner.close();
         socket.close();
+
+
+        Selector selector = Selector.open();
+        SocketChannel socketChannel = SocketChannel.open();
+        //将管道设置成非阻塞
+        socketChannel.configureBlocking(false);
+        int interest = SelectionKey.OP_ACCEPT | SelectionKey.OP_CONNECT;
+        /**
+         * selectionKey对象提供了channel和selector的一种绑定的状态
+         */
+        SelectionKey key = socketChannel.register(selector, interest);
+        //这个是一个阻塞方法，如果没有同道处于已经就绪的状态，那么线程会阻塞到该方法上。
+        selector.select();
+
     }
 }
--- src/net/ServerDemo.java
+++ src/net/ServerDemo.java
@@ -2,10 +2,69 @@ package net;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class ServerDemo {
+
+    /**
+     * 一个完整的nio的例子
+     * @throws IOException
+     */
+    public void selector() throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(2 * 1024);
+        Selector selector = Selector.open();
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        //这里为什么需要用socket对象来绑定监听，而没有用ServerSocket来绑定
+        serverSocketChannel.socket().bind(new InetSocketAddress(55333));
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        while (true) {
+            int selectionNum = selector.select();
+            if (selectionNum <= 0) {
+                continue;
+            }
+            Set<SelectionKey> set = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = set.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                //说明服务器socket已经接受
+                if ((key.readyOps() & SelectionKey.OP_ACCEPT) == SelectionKey.OP_ACCEPT) {
+                    //存在继承的关系，所以可以直接转型
+                    ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+                    //这里已经得到了服务器的channel下一步是为读数据作准备
+                    SocketChannel sc = ssc.accept();
+                    //开始读取数据
+                    sc.configureBlocking(false);
+                    sc.register(selector, SelectionKey.OP_READ);
+                    iterator.remove();
+                }
+                //表示已经做好了读的准备
+                else if ((key.readyOps() & SelectionKey.OP_READ) == SelectionKey.OP_READ) {
+                    SocketChannel sc = (SocketChannel) key.channel();
+                    while (true) {
+                        buffer.clear();
+                        int count = sc.read(buffer);
+                        if (count < 0) {
+                            break;
+                        }
+                    }
+                    buffer.flip();
+                    iterator.remove();
+                }
+            }
+        }
+
+    }
+
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(9999);
         System.out.println("等待客户连接");
